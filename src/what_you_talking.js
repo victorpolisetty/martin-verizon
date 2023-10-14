af3169f370b113ce16814116811d65f8c3ff3828
Hannah Hardy <hannahohardy@gmail.com>
2023-10-14 16:22:32 -0400

    First 3 sequences working correct

--- src/what_you_talking.js
+++ src/what_you_talking.js
@@ -2,21 +2,11 @@ import React, { useState } from 'react';
 import './what_you_talking.css'; // Import your CSS file
 
 function ChatBot() {
-  const [showOptions, setShowOptions] = useState(false);
-  const [selectedLines, setSelectedLines] = useState(1); // Store the selected number of lines (default: 1)
-  const [step, setStep] = useState(1); // Current step in the process
-  const [smartWatch, setSmartWatch] = useState(0); // Number of Smart Watches
-  const [jetpackHotspot, setJetpackHotspot] = useState(0); // Number of Jetpack Hotspots
-  const [cellularTablet, setCellularTablet] = useState(0); // Number of Cellular Tablets
-
-  const handleClick = () => {
-    if (step === 1) {
-      setShowOptions(true);
-    } else {
-      // Handle next step
-      // For example, you can submit the data or navigate to the next step in your application
-    }
-  };
+  const [selectedLines, setSelectedLines] = useState(1);
+  const [step, setStep] = useState(1);
+  const [smartWatch, setSmartWatch] = useState(0);
+  const [jetpackHotspot, setJetpackHotspot] = useState(0);
+  const [cellularTablet, setCellularTablet] = useState(0);
 
   const handleSliderChange = (event) => {
     setSelectedLines(parseInt(event.target.value));
@@ -25,74 +15,88 @@ function ChatBot() {
   const handleNext = () => {
     if (step === 1) {
       setStep(2);
+    } else if (step === 2) {
+      setStep(3);
     }
   };
 
   const handleBack = () => {
-    if (step === 2) {
-      setStep(1);
+    if (step > 1) {
+      setStep(step - 1);
+    }
+  };
+
+  const getScreen = () => {
+    if (step === 1) {
+      return (
+        <div>
+          <h1>Welcome! I am your personal Verizon agent Martin here to help you find the plan to match your needs.</h1>
+          <button onClick={handleNext}>Let's get started</button>
+        </div>
+      );
+    } else if (step === 2) {
+      return (
+        <div>
+          <p>How many lines would you like to have on your plan?</p>
+          <input
+            type="range"
+            min="1"
+            max="5"
+            value={selectedLines}
+            onChange={handleSliderChange}
+          />
+          <span>{selectedLines} Line(s)</span>
+          <button onClick={handleNext}>Next</button>
+        </div>
+      );
+    } else if (step === 3) {
+      return (
+        <div>
+          <p>How many of each connected device are you including in your plan?</p>
+          <div>
+            <p>Smart Watch</p>
+            <input
+              type="range"
+              min="0"
+              max="5"
+              value={smartWatch}
+              onChange={(e) => setSmartWatch(parseInt(e.target.value))}
+            />
+            <span>{smartWatch}</span>
+          </div>
+          <div>
+            <p>Jetpack Hotspot</p>
+            <input
+              type="range"
+              min="0"
+              max="5"
+              value={jetpackHotspot}
+              onChange={(e) => setJetpackHotspot(parseInt(e.target.value))}
+            />
+            <span>{jetpackHotspot}</span>
+          </div>
+          <div>
+            <p>Cellular Tablet</p>
+            <input
+              type="range"
+              min="0"
+              max="5"
+              value={cellularTablet}
+              onChange={(e) => setCellularTablet(parseInt(e.target.value))}
+            />
+            <span>{cellularTablet}</span>
+          </div>
+          <button onClick={handleNext}>Next</button>
+        </div>
+      );
     }
   };
 
   return (
     <div className="ChatBot">
       <div className="chat-content">
-        {step === 1 && showOptions ? (
-          <div>
-            <p>How many lines would you like to have on your plan?</p>
-            <input
-              type="range"
-              min="1"
-              max="5"
-              value={selectedLines}
-              onChange={handleSliderChange}
-            />
-            <span>{selectedLines} Line(s)</span>
-          </div>
-        ) : step === 2 ? (
-          <div>
-            <p>How many of each connected device are you including in your plan?</p>
-            <div>
-              <p>Smart Watch</p>
-              <input
-                type="range"
-                min="0"
-                max="5"
-                value={smartWatch}
-                onChange={(e) => setSmartWatch(parseInt(e.target.value))}
-              />
-              <span>{smartWatch}</span>
-            </div>
-            <div>
-              <p>Jetpack Hotspot</p>
-              <input
-                type="range"
-                min="0"
-                max="5"
-                value={jetpackHotspot}
-                onChange={(e) => setJetpackHotspot(parseInt(e.target.value))}
-              />
-              <span>{jetpackHotspot}</span>
-            </div>
-            <div>
-              <p>Cellular Tablet</p>
-              <input
-                type="range"
-                min="0"
-                max="5"
-                value={cellularTablet}
-                onChange={(e) => setCellularTablet(parseInt(e.target.value))}
-              />
-              <span>{cellularTablet}</span>
-            </div>
-          </div>
-        ) : (
-          <h1>Welcome! I am your personal Verizon agent Martin here to help you find the plan to match your needs.</h1>
-        )}
-        <button onClick={handleClick}>
-          {step === 2 ? 'Submit' : 'Next'}
-        </button>
-        {step === 2 && (
+        {getScreen()}
+        {step > 1 && (
           <button onClick={handleBack}>Back</button>
         )}
       </div>
