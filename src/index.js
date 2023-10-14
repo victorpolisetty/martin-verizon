06f051790bf6af58198cfb30ce6462726a5feed4
andrewruales <andrewruales@gmail.com>
2023-10-14 14:15:20 -0400

    removed unnecessary files and added new files to work out of

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import ChatBot from './what_you_talking'; // Import the Website component
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ChatBot /> {/* Render the Website component */}
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/what_you_talking.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './what_you_talking.css'; // Import your CSS file
+
+function ChatBot() {
+  return (
+    <div className="ChatBot">
+      {/* Your website content goes here */}
+    </div>
+  );
+}
+
+export default ChatBot;
