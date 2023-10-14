4e5959d79a07c8952f07b7fd3ffbf051981d54d2
jonathanzhang53 <jonathanzhang53@gmail.com>
2023-10-14 13:59:38 -0400

    backend

--- backend/create_jsonl_from_text.py
+++ backend/create_jsonl_from_text.py
@@ -1,11 +1,13 @@
 import json
 
+SYSTEM_PROMPT = "Your name is Martin. You are a factual chatbot that assists prospective Verizon customers during exploration of Verizon's products and services."
+
 # read the text file
 with open("assets/prompts.txt", "r", encoding='utf-8') as f:
     lines = f.readlines()
 
 # prepare the JSONL file
-with open("planData.jsonl", "w", encoding='utf-8') as f:
+with open("assets/planData.jsonl", "w", encoding='utf-8') as f:
     # loop over the lines two at a time
     for i in range(0, len(lines), 2):
         user_content = lines[i].strip()
@@ -13,7 +15,7 @@ with open("planData.jsonl", "w", encoding='utf-8') as f:
 
         data = {
             "messages": [
-                {"role": "system", "content": "Martin is a factual chatbot that assists prospective Verizon customers during exploration of Verizon's products and services."},
+                {"role": "system", "content": SYSTEM_PROMPT},
                 {"role": "user", "content": user_content},
                 {"role": "assistant", "content": assistant_content}
             ]
--- backend/gpt_poc.py
+++ backend/gpt_poc.py
@@ -2,17 +2,22 @@ import os
 import openai
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
-messages = [ {"role": "system", "content":  
-              "You are a intelligent assistant."} ] 
+messages = [{
+                "role": "system",
+                "content": "You are a intelligent assistant."
+            }] 
+
 while True: 
-    message = input("User : ") 
+    message = input("User : ")
+
     if message: 
         messages.append( 
             {"role": "user", "content": message}, 
         ) 
         chat = openai.ChatCompletion.create( 
             model="gpt-4", messages=messages
-        ) 
+        )
+    
     reply = chat.choices[0].message.content 
     print(f"ChatGPT: {reply}")
-    messages.append({"role": "assistant", "content": reply}) 
+    messages.append({"role": "assistant", "content": reply})
