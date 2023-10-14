1102007e7fc910ac5154766a5bffc3e9b5e01d2a
Hannah Hardy <hannahohardy@gmail.com>
2023-10-14 15:17:19 -0400

    initial screen with text and button to start

--- src/what_you_talking.js
+++ src/what_you_talking.js
@@ -4,7 +4,10 @@ import './what_you_talking.css'; // Import your CSS file
 function ChatBot() {
   return (
     <div className="ChatBot">
-      {/* Your website content goes here */}
+      <div className="chat-content">
+        <h1>Welcome! I am your personal Verizon agent Martin here to help you find the plan to match your needs.</h1>
+        <button>Let's get started</button>
+      </div>
     </div>
   );
 }
